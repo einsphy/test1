388bf3e395d1f0273814a3918b983cba57c3abec
einsphy <einsphy@einsphydemacbook-air.local>
2016-02-19 17:17:44 +0800

    初始化项目

---
+++ EaseTalk/EaseTalk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EaseTalk
+//
+//  Created by einsphy on 16/2/19.
+//  Copyright © 2016年 einsphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EaseTalk/EaseTalk/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EaseTalk
+//
+//  Created by einsphy on 16/2/19.
+//  Copyright © 2016年 einsphy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
